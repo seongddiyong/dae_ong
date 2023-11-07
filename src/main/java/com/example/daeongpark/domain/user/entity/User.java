3c2a6b2fdbac70a3c0e680d16f965f9ff4c16684
seongddiyong <kodd1102@naver.com>
2023-11-07 23:43:23 +0900

    [feat/start/#1] make User entity

---
+++ src/main/java/com/example/daeongpark/domain/user/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.daeongpark.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String nickname;
+}
