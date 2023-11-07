10e87a3b4495961f3482731bd6c6b9bf0b0fbbf1
seongddiyong <kodd1102@naver.com>
2023-11-07 23:08:15 +0900

    initial

---
+++ src/main/java/com/example/daeongpark/DaeongParkApplication.java
@@ -0,0 +1,13 @@
+package com.example.daeongpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaeongParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaeongParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/daeongpark/DaeongParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daeongpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaeongParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
